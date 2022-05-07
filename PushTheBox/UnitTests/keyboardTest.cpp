a52c52166140c378140af6421ed8d09db9cbff6e
Michanowski9 <m.sojka99@gmail.com>
2022-05-07 13:56:12 +0200

    new tests for KeyboardTest

--- PushTheBox/UnitTests/keyboardTest.cpp
+++ PushTheBox/UnitTests/keyboardTest.cpp
@@ -9,7 +9,7 @@ public:
 	MOCK_METHOD(const bool, GetKeyState, (int), (override));
 };
 
-TEST(KeyboardTest, KeyIsDown_GetKeyDown_ReturnTrue) {
+TEST(KeyboardTest, GetKeyDown_KeyIsDown_ReturnTrue) {
 	//Given
 	SystemKeyboardMock systemKeyboardMock;
 	EXPECT_CALL(systemKeyboardMock, GetKeyState('A'))
@@ -23,8 +23,7 @@ TEST(KeyboardTest, KeyIsDown_GetKeyDown_ReturnTrue) {
 	//Then
 	ASSERT_TRUE(result);
 }
-
-TEST(KeyboardTest, KeyIsUp_GetKeyDown_ReturnFalse) {
+TEST(KeyboardTest, GetKeyDown_KeyIsUp_ReturnFalse) {
 	//Given
 	SystemKeyboardMock systemKeyboardMock;
 	EXPECT_CALL(systemKeyboardMock, GetKeyState('A'))
@@ -38,8 +37,7 @@ TEST(KeyboardTest, KeyIsUp_GetKeyDown_ReturnFalse) {
 	//Then
 	ASSERT_FALSE(result);
 }
-
-TEST(KeyboardTest, KeyIsDown_GetKeyUp_ReturnFalse) {
+TEST(KeyboardTest, GetKeyUp_KeyIsDown_ReturnFalse) {
 	//Given
 	SystemKeyboardMock systemKeyboardMock;
 	EXPECT_CALL(systemKeyboardMock, GetKeyState('A'))
@@ -53,8 +51,7 @@ TEST(KeyboardTest, KeyIsDown_GetKeyUp_ReturnFalse) {
 	//Then
 	ASSERT_FALSE(result);
 }
-
-TEST(KeyboardTest, KeyIsUp_GetKeyUp_ReturnTrue) {
+TEST(KeyboardTest, GetKeyUp_KeyIsUp_ReturnTrue) {
 	//Given
 	SystemKeyboardMock systemKeyboardMock;
 	EXPECT_CALL(systemKeyboardMock, GetKeyState('A'))
@@ -68,5 +65,78 @@ TEST(KeyboardTest, KeyIsUp_GetKeyUp_ReturnTrue) {
 	//Then
 	ASSERT_TRUE(result);
 }
+TEST(KeyboardTest, GetKeyPressed_KeyIsUpThanDown_ReturnTrue) {
+	//Given
+	SystemKeyboardMock systemKeyboardMock;
+	EXPECT_CALL(systemKeyboardMock, GetKeyState('A'))
+		.WillOnce(Return(0))
+		.WillRepeatedly(Return(1));
 
-TEST()
+	Keyboard keyboard = Keyboard(systemKeyboardMock);
+
+	//When
+	auto result1 = keyboard.GetKeyPressed('A');
+	auto result2 = keyboard.GetKeyPressed('A');
+	auto result3 = keyboard.GetKeyPressed('A');
+
+	//Then
+	EXPECT_FALSE(result1);
+	EXPECT_TRUE(result2);
+	EXPECT_FALSE(result3);
+}
+TEST(KeyboardTest, GetKeyPressed_KeyIsDownThanUp_ReturnFalse) {
+	//Given
+	SystemKeyboardMock systemKeyboardMock;
+	EXPECT_CALL(systemKeyboardMock, GetKeyState('A'))
+		.WillOnce(Return(1))
+		.WillRepeatedly(Return(0));
+
+	Keyboard keyboard = Keyboard(systemKeyboardMock);
+
+	//When
+
+	//this is to skip not pressed state when constructing object 
+	//(by default is setting to 0 in array)
+	keyboard.GetKeyPressed('A');
+
+	auto result1 = keyboard.GetKeyPressed('A');
+	auto result2 = keyboard.GetKeyPressed('A');
+
+	//Then
+	EXPECT_FALSE(result1);
+	EXPECT_FALSE(result2);
+}
+TEST(KeyboardTest, GetKeyReleased_KeyIsDownThanUp_ReturnTrue) {
+	//Given
+	SystemKeyboardMock systemKeyboardMock;
+	EXPECT_CALL(systemKeyboardMock, GetKeyState('A'))
+		.WillOnce(Return(1))
+		.WillOnce(Return(0));
+
+	Keyboard keyboard = Keyboard(systemKeyboardMock);
+
+	//When
+	auto result1 = keyboard.GetKeyReleased('A');
+	auto result2 = keyboard.GetKeyReleased('A');
+
+	//Then
+	EXPECT_FALSE(result1);
+	EXPECT_TRUE(result2);
+}
+TEST(KeyboardTest, GetKeyReleased_KeyIsUpThanDown_ReturnFalse) {
+	//Given
+	SystemKeyboardMock systemKeyboardMock;
+	EXPECT_CALL(systemKeyboardMock, GetKeyState('A'))
+		.WillOnce(Return(0))
+		.WillOnce(Return(1));
+
+	Keyboard keyboard = Keyboard(systemKeyboardMock);
+
+	//When
+	auto result1 = keyboard.GetKeyReleased('A');
+	auto result2 = keyboard.GetKeyReleased('A');
+
+	//Then
+	EXPECT_FALSE(result1);
+	EXPECT_FALSE(result2);
+}
