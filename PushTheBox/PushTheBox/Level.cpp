9fa36eb823c07df0488794ef7a668bb96b7b9b6b
Michanowski9 <m.sojka99@gmail.com>
2022-05-11 18:10:00 +0200

    added pushing boxes out of targer

--- PushTheBox/PushTheBox/Level.cpp
+++ PushTheBox/PushTheBox/Level.cpp
@@ -30,7 +30,7 @@ const bool Level::IsEmptyCell(int x, int y) const
 
 const bool Level::IsBoxOnField(int x, int y) const
 {
-	if (map[x][y] == CELL::BOX) {
+	if (map[x][y] == CELL::BOX || map[x][y] == CELL::BOX_ON_TARGET) {
 		return true;
 	}
 	return false;
@@ -41,6 +41,10 @@ void Level::MoveBoxUp(int boxX, int boxY)
 	if (map[boxX][boxY - 1] == CELL::TARGET) {
 		SetField(boxX, boxY, CELL::EMPTY);
 		SetField(boxX, boxY - 1, CELL::BOX_ON_TARGET);
+	}
+	else if (map[boxX][boxY] == CELL::BOX_ON_TARGET) {
+		SetField(boxX, boxY, CELL::TARGET);
+		SetField(boxX, boxY - 1, CELL::BOX);
 	}
 	else if (map[boxX][boxY - 1] == CELL::EMPTY) {
 		SetField(boxX, boxY, CELL::EMPTY);
@@ -54,6 +58,10 @@ void Level::MoveBoxDown(int boxX, int boxY)
 		SetField(boxX, boxY, CELL::EMPTY);
 		SetField(boxX, boxY + 1, CELL::BOX_ON_TARGET);
 	}
+	else if (map[boxX][boxY] == CELL::BOX_ON_TARGET) {
+		SetField(boxX, boxY, CELL::TARGET);
+		SetField(boxX, boxY + 1, CELL::BOX);
+	}
 	else if (map[boxX][boxY + 1] == CELL::EMPTY) {
 		SetField(boxX, boxY, CELL::EMPTY);
 		SetField(boxX, boxY + 1, CELL::BOX);
@@ -66,6 +74,10 @@ void Level::MoveBoxLeft(int boxX, int boxY)
 		SetField(boxX, boxY, CELL::EMPTY);
 		SetField(boxX - 1, boxY, CELL::BOX_ON_TARGET);
 	}
+	else if (map[boxX][boxY] == CELL::BOX_ON_TARGET) {
+		SetField(boxX, boxY, CELL::TARGET);
+		SetField(boxX - 1, boxY, CELL::BOX);
+	}
 	else if (map[boxX - 1][boxY] == CELL::EMPTY) {
 		SetField(boxX, boxY, CELL::EMPTY);
 		SetField(boxX - 1, boxY, CELL::BOX);
@@ -77,6 +89,10 @@ void Level::MoveBoxRight(int boxX, int boxY)
 	if (map[boxX + 1][boxY] == CELL::TARGET) {
 		SetField(boxX, boxY, CELL::EMPTY);
 		SetField(boxX + 1, boxY, CELL::BOX_ON_TARGET);
+	}
+	else if (map[boxX][boxY] == CELL::BOX_ON_TARGET) {
+		SetField(boxX, boxY, CELL::TARGET);
+		SetField(boxX + 1, boxY, CELL::BOX);
 	}
 	else if (map[boxX + 1][boxY] == CELL::EMPTY) {
 		SetField(boxX, boxY, CELL::EMPTY);
@@ -123,6 +139,8 @@ void Level::LoadDefaultMap()
 		}
 	}
 	map[3][3] = CELL::BOX;
+	map[9][3] = CELL::BOX;
+	map[3][4] = CELL::TARGET;
 	map[10][4] = CELL::TARGET;
 	playerPtr->SetPosition(5, 5);
 }
